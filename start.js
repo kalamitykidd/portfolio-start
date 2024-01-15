47ee3209ba0b3f4b2ba016f9ce5e8f4225e2c912
kalamitykidd <156458678+kalamitykidd@users.noreply.github.com>
2024-01-14 22:24:28 -0700

    Create start.js

---
+++ start.js
@@ -0,0 +1,8 @@
+/* Regan Kidd
+    01/13/2024
+                */
+console.log("riddle")
+
+var riddle= prompt("What is so delicate that saying its name breaks it?");
+riddle += 3;
+console.log("The riddle variable \n \nis equal to:" +riddle);
