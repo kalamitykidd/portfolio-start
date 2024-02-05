38286f1a71df174062ff208d6e1b164224c45b86
kalamitykidd <regan.seaton1993@gmail.com>
2024-02-05 00:30:10 -0500

    button change

--- domscript.js
+++ domscript.js
@@ -1,24 +1,15 @@
 
-		("#add").addEventListener("click"), () => function (){
-
-            const newParagraph = document.createElement("p");
-            
-            const text = document.createTextNode("Testing paragraph");
-            newParagraph.appendChild(text);
-        
-            
-        const firstParagraph = ("p");
-        const parent = firstParagraph.parentNode;
-
-        parent.insertBefore(newParagraph, firstParagraph.nextElementSibling);
-        function changeColors() {
+		$("#changeButton").addEventListener("click"), () => function changeColors() {
             document.body.style.backgroundColor = "ivory"; 
             paraDiv.style.backgroundColor = "green";  
             paraDiv.style.color = "black";
-        }
+        
         
         orChangeButton = document.createElement("button");
             colorChangeButton.textContent = "Change Colors";
             colorChangeButton.onclick = changeColors;
             document.body.appendChild(colorChangeButton);
         }
+
+           
+        
