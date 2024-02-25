17514d01c2c8f5276823938f2bc720c7b87929bd
kalamitykidd <regan.seaton1993@gmail.com>
2024-02-25 08:11:18 -0700

    add objects.html

---
+++ Unit 2/lib_naming.js
@@ -0,0 +1,8 @@
+class Name {
+    constructor(){
+        this.firstname = Regan;
+        this.lastname = Kidd;
+    }
+}
+
+console.log(Name)
---
+++ Unit 2/objects.js
@@ -0,0 +1,11 @@
+var human1 = new Name();
+human1.firstname = Tom;
+human1.lastname = Jones;
+
+var human2 = new Name();
+human2.firstname = Lisa;
+human2.lastname = Landry;
+
+console.log(human1);
+console.log(human2);
+
